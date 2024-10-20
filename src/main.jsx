78d0097953b491587edcfdd3668f244a2422bebf
watercubz <eurysosagarcia@gmail.com>
2024-10-20 14:09:30 -0400

    fix-main: Error with the new version of react router I deleted the browserRouter that was giving the error

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import { StrictMode } from "react";
-import ReactDom from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
