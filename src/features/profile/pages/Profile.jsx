8cdc5ce5e3012634cf06084fe0fe4264a1db8cab
watercubz <eurysosagarcia@gmail.com>
2024-10-21 13:26:06 -0400

    feat-profile: User profile structuring not yet available

---
+++ src/features/profile/pages/Profile.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function Profile() {
+  const navigate = useNavigate();
+
+  const handleProfile = () => {
+    navigate('/Profile');
+  };
+
+  return (
+    <button
+      onClick={handleProfile}
+      className="cursor-pointer text-white hover:text-green-600"
+    >
+      Profile
+    </button>
+  );
+}
