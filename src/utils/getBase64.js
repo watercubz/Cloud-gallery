bced7480c4703477e56f01d2aaee2df5a0a398a5
watercubz <eurysosagarcia@gmail.com>
2024-11-01 12:54:28 -0400

    feat: Comment agg for getBase64

--- src/utils/getBase64.js
+++ src/utils/getBase64.js
@@ -1,8 +1,8 @@
-// getBase64 permite leer informacion guardada en buffer y si hay una error lo eyectamos
+// getBase64 allows reading information stored in buffer and if there is an error we eject it
 export const getBase64 = (file) =>
   new Promise(function (resolve, reject) {
     let render = new FileReader();
     render.readAsDataURL(file);
     render.onload = () => resolve(render.result);
-    render.onerror = (error) => reject("error", error);
+    render.onerror = (error) => reject('error', error);
   });
