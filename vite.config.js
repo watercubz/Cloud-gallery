cc7d0c0081870291c8f8ed5fd5908a3645af7b5a
watercubz <eurysosagarcia@gmail.com>
2024-11-05 16:12:42 -0400

    feat-test: Initializing the environment to integrate test

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://api.cloudinary.com",
+      '/api': {
+        target: 'https://api.cloudinary.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
   },
 });
