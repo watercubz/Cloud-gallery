7d7381726f6b9313135f3112d49991248321a202
watercubz <eurysosagarcia@gmail.com>
2024-11-06 14:00:03 -0400

    feat-gallery: improved performance and loading of css agg some additional settings for images

--- src/features/gallery/components/GalleryComponent.jsx
+++ src/features/gallery/components/GalleryComponent.jsx
@@ -2,17 +2,56 @@
 import Loading from '../../ui/Loading';
 import useFecthCloud from '../../../hooks/useFecthcloud';
 
-const GalleryImage = ({ imageUrl }) => {
-  return (
-    <div className="inline-block">
-      <img
-        src={imageUrl}
-        alt="Gallery Image"
-        className="h-[300px] w-full max-w-[300px] rounded-lg object-cover m-2"
-      />
+const GalleryImage = ({ imageUrl }) => (
+  <div className="inline-block">
+    <img
+      src={imageUrl}
+      alt="Gallery Image"
+      width="300"
+      height="300"
+      className="h-[300px] w-full max-w-[300px] rounded-lg object-cover m-2"
+    />
+  </div>
+);
+
+const ConfirmationDialog = ({ onConfirm, onCancel }) => (
+  <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur confirm-dialog">
+    <div className="relative px-4 min-h-screen md:flex md:items-center md:justify-center">
+      <div className="opacity-25 w-full h-full absolute z-10 inset-0"></div>
+      <div className="bg-gray-800 rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg">
+        <div className="md:flex items-center">
+          <div className="rounded-full border border-gray-300 flex items-center justify-center w-16 h-16 flex-shrink-0 mx-auto">
+            <i className="bx bx-error text-3xl">&#9888;</i>
+          </div>
+          <div className="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
+            <p className="font-bold text-white">AVISO</p>
+            <p className="text-sm text-white mt-1">
+              Al subir tus imágenes, aceptas que todos puedan verlas e incluso
+              modificarlas. Cloud Gallery no se hace responsable del uso que se
+              les dé a dichas imágenes. Al hacer clic, aceptas esta política.
+            </p>
+          </div>
+        </div>
+        <div className="text-center md:text-right mt-4 md:flex md:justify-end gap-5">
+          <button
+            className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-red-500 text-gray-100 hover:text-red-200 rounded-lg font-semibold text-sm md:ml-2 md:order-2"
+            onClick={onConfirm}
+            aria-label="Confirmar aceptación"
+          >
+            Confirmar
+          </button>
+          <button
+            className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-gray-700 text-white rounded-lg font-semibold text-sm md:ml-2 md:order-2 hover:text-gray-300"
+            onClick={onCancel}
+            aria-label="Denegar aceptación"
+          >
+            Denegar
+          </button>
+        </div>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default function GalleryComponent() {
   const { images, loading, agreed, handleAgreement, handleCancel } =
@@ -21,46 +60,15 @@ export default function GalleryComponent() {
   return (
     <>
       {!agreed ? (
-        <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur confirm-dialog">
-          <div className="relative px-4 min-h-screen md:flex md:items-center md:justify-center">
-            <div className=" opacity-25 w-full h-full absolute z-10 inset-0"></div>
-            <div className="bg-gray-800 rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg">
-              <div className="md:flex items-center">
-                <div className="rounded-full border border-gray-300 flex items-center justify-center w-16 h-16 flex-shrink-0 mx-auto">
-                  <i className="bx bx-error text-3xl ">&#9888;</i>
-                </div>
-                <div className="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
-                  <p className="font-bold text-white">AVISO</p>
-                  <p className="text-sm text-white mt-1">
-                    Al subir tus imágenes, aceptas que todos puedan verlas e
-                    incluso modificarlas. Cloud Gallery no se hace responsable
-                    del uso que se les dé a dichas imágenes. Al hacer clic,
-                    aceptas esta política.
-                  </p>
-                </div>
-              </div>
-              <div className="text-center md:text-right mt-4 md:flex md:justify-end gap-5">
-                <button
-                  className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-red-500 text-gray-100 hover:text-red-200 rounded-lg font-semibold text-sm md:ml-2 md:order-2"
-                  onClick={handleAgreement}
-                >
-                  confirmar
-                </button>
-                <button
-                  className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-gray-700 text-white rounded-lg font-semibold text-sm md:ml-2 md:order-2 hover:text-gray-300"
-                  onClick={handleCancel}
-                >
-                  Denegar
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ConfirmationDialog
+          onConfirm={handleAgreement}
+          onCancel={handleCancel}
+        />
       ) : (
         <div className="flex flex-wrap justify-center">
           {loading ? (
             <Loading /> // Mensaje de carga
-          ) : images.length > 0 ? (
+          ) : Array.isArray(images) && images.length > 0 ? (
             images.map((image) => (
               <GalleryImage key={image.public_id} imageUrl={image.secure_url} />
             ))
--- src/features/gallery/pages/Home.jsx
+++ src/features/gallery/pages/Home.jsx
@@ -1,46 +1,24 @@
-import { useState, useEffect } from 'react';
-import toast from 'react-hot-toast';
 import Chess from '../../../services/3D/Chess';
 import Hero from '../../Layouts/Hero';
-import Loading from '../../ui/Loading';
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-      toast.success('Objeto 3D cargado 💚');
-    }, 200);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) <p className="bg-red-700">Cargando....</p>;
-
   return (
     <>
-      {loading ? (
-        <div className="flex items-center justify-center min-h-screen">
-          <Loading />
-        </div>
-      ) : (
-        <div className="relative h-screen w-full">
-          {/* Fondo radial */}
-          <div className="absolute inset-0 -z-10 bg-black bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
-          <div className="absolute left-0 right-0 top-0 m-auto h-[310px] w-[310px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)] blur-[100px]" />
+      <div className="relative h-screen w-full">
+        {/* Fondo radial */}
+        <div className="absolute inset-0 -z-10 bg-black bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
+        <div className="absolute left-0 right-0 top-0 m-auto h-[310px] w-[310px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)] blur-[100px]" />
 
-          {/* Contenido centrado */}
-          <div className="flex flex-col items-center justify-center h-full z-30 relative p-4">
-            <Hero />
-          </div>
-
-          {/* Asegúrate de que el componente Chess sea responsivo */}
-          <div className="hidden xl:flex justify-center items-center mt-4">
-            <Chess className="w-full max-w-md" />
-          </div>
+        {/* Contenido centrado */}
+        <div className="flex flex-col items-center justify-center h-full z-30 relative p-4">
+          <Hero />
         </div>
-      )}
+
+        {/* Asegúrate de que el componente Chess sea responsivo */}
+        <div className="hidden xl:flex justify-center items-center mt-4">
+          <Chess className="w-full max-w-md" />
+        </div>
+      </div>
     </>
   );
 }
