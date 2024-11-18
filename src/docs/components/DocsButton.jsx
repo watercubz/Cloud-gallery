8d6a38877228e364d35121c47304d22c74f999be
watercubz <eurysosagarcia@gmail.com>
2024-11-18 17:44:57 -0400

    fix: docs error

---
+++ src/docs/components/DocsButton.jsx
@@ -0,0 +1,9 @@
+export default function Docs() {
+  return (
+    <a href="https://github.com/watercubz/MTAAI-core" target="_blank">
+      <span className="text-black  dark:text-white hover:text-blue-600">
+        <a className=" hover:text-blue-600">Docs</a>
+      </span>
+    </a>
+  );
+}
