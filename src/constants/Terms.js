5b0751b58d2700abaeea84a16398634f0d7388ff
watercubz <eurysosagarcia@gmail.com>
2024-11-18 17:57:34 -0400

    feat: agg constants for terms and privacy

---
+++ src/constants/Terms.js
@@ -0,0 +1,57 @@
+export const Terms = [
+  {
+    title: 'Descripción del Servicio',
+    content: `Eres responsable de mantener la confidencialidad de tu cuenta y
+          contraseña. Debes aceptar la responsabilidad de todas las
+          actividades que ocurran bajo tu cuenta. No puedes usar el nombre de
+          otra persona sin autorización. Nos reservamos el derecho a negar el
+          servicio, cancelar cuentas, o remover contenido a nuestra
+          discreción.`,
+  },
+  {
+    title: 'Responsabilidades de la Cuenta',
+    content: ` La privacidad de tu información es importante para nosotros. Toda la
+          información que proporciones al crear una cuenta y usar el servicio
+          será recopilada y protegida de acuerdo con nuestra política de
+          privacidad. Al crear una cuenta, consientes el uso y transferencia
+          de esta información para el almacenamiento, procesamiento y mejora
+          del servicio.`,
+  },
+  {
+    title: 'Privacidad de la Información de la Cuenta',
+    content: `La privacidad de tu información es importante para nosotros. Toda la
+          información que proporciones al crear una cuenta y usar el servicio
+          será recopilada y protegida de acuerdo con nuestra política de
+          privacidad. Al crear una cuenta, consientes el uso y transferencia
+          de esta información para el almacenamiento, procesamiento y mejora
+          del servicio.`,
+  },
+  {
+    title: 'Proceso de Pago',
+    content: `El proceso de pago se realiza a través de PayPal. No se realizan
+          devoluciones una vez completado el pago. Al realizar un pago,
+          aceptas los términos establecidos por MTAAI-core y entiendes que
+          todos los cargos son definitivos.`,
+  },
+  {
+    title: '  Recopilación de Información',
+    content: `MTAAI-core recopilará información relacionada con el uso de la
+          plataforma y el rendimiento de tu publicidad. Esta información
+          incluye datos de usuario y estadísticas, y se utilizará para mejorar
+          los servicios proporcionados y optimizar la efectividad de la
+          publicidad.`,
+  },
+  {
+    title: 'Conducta Prohibida',
+    content: ` 
+    - Violar cualquier ley o regulación aplicable.
+    - Participar en conductas fraudulentas, amenazantes, difamatorias, o ofensivas.
+    - Comprometer la seguridad de la cuenta de MTAAI-core o la de otros usuarios.
+    - Intentar hacerse pasar por el equipo de MTAAI-core o cualquier otro usuario.
+    `,
+  },
+  {
+    confirm: `Al utilizar MTAAI-core, confirmas que aceptas estos términos y
+          condiciones. Si tienes alguna pregunta, no dudes en contactarnos.`,
+  },
+];
