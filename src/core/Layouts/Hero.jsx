6c524ec18a4497c055768a83ec8bfc2f1596f7fe
watercubz <eurysosagarcia@gmail.com>
2024-11-26 21:15:03 -0400

    feat: Added translation settings for the discount banner

--- src/core/Layouts/Hero.jsx
+++ src/core/Layouts/Hero.jsx
@@ -43,14 +43,8 @@ export default function Hero() {
                       </svg>
                     </span>
                     <p className="ml-3 truncate font-medium text-white">
-                      <span className="md:hidden">
-                        {' '}
-                        ¡Disfruta este mes de premium gratis! 🎉
-                      </span>
-                      <span className="hidden md:inline">
-                        ¡Disfruta este mes de premium totalmente gratis por
-                        nuestro gran lanzamiento! 🎉
-                      </span>
+                      <span className="md:hidden"> {t('discountMovil')}</span>
+                      <span className="hidden md:inline">{t('discount')}</span>
                     </p>
                   </div>
                   <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
@@ -58,7 +52,7 @@ export default function Hero() {
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
                       href="/pricing"
                     >
-                      Empezar
+                      {t('Start')}
                     </a>
                   </div>
                   <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
