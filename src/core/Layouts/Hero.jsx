32d5aa0b6f701e31cb3f87e16f6b115511243da5
Eury Sosa Garcia <112292926+watercubz@users.noreply.github.com>
2024-12-04 13:02:04 -0400

    Update Hero.jsx

--- src/core/Layouts/Hero.jsx
+++ src/core/Layouts/Hero.jsx
@@ -215,7 +215,7 @@ export default function Hero() {
               </p>
               <div className="px-8 sm:items-center sm:justify-center sm:px-0 sm:space-x-5 sm:flex mt-9">
                 <a
-                  href="/app"
+                  href="#"
                   className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl font-pj hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   role="button"
                 >
